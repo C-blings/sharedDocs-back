9ffed531d01f75556db9e94b7e973537c3888283
Paul Nikulshin <aksum@aksum.aksum>
2023-10-23 18:12:03 +0300

    fix HttpRequestParser

--- src/web/helpers/HttpRequestParser.hpp
+++ src/web/helpers/HttpRequestParser.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
 #include "../models/HttpRequest.hpp"
+#include <ostream>
 #include <sstream>
+#include <string>
 #include <vector>
 #include <unordered_map>
 #include <boost/algorithm/string.hpp>
@@ -9,8 +11,14 @@
 namespace web_layout{
 
     class HttpRequestParser{
+        private:
+            static inline std::unordered_map<std::string, Method> string_to_method = {
+                {"GET", Method::GET},
+                {"POST", Method::POST},
+                {"PUT", Method::PUT},
+                {"DELETE", Method::DELETE}
+            };
         public:
-            
             static HttpRequest GetHttpRequest(const std::string& request){
                 std::vector<std::string> request_lines;
                 boost::split(request_lines, request, boost::is_any_of("\n\r"), boost::token_compress_on);
@@ -18,30 +26,20 @@ namespace web_layout{
 				std::vector<std::string> method_and_path;
 				boost::split(method_and_path, request_lines[0], boost::is_any_of(" "), boost::token_compress_on);
 
-				Method method;
-				if(method_and_path[0] == "GET") {
-						method = Method::GET;
-				} else if(method_and_path[0] == "POST") {
-						method = Method::POST;
-				} else if(method_and_path[0] == "PUT") {
-						method = Method::PUT;
-				} else if(method_and_path[0] == "DELETE") {
-						method = Method::DELETE;
-				}
-
+				Method method = string_to_method[method_and_path[0]];
 				std::string path = method_and_path[1];
 				std::unordered_map<std::string, std::string> headers;
 				std::string body;
 
 				int i;
 				for(i = 1; i < request_lines.size(); ++i) {
-						if(request_lines[i] == "") {
+						if(request_lines[i].empty()) {
 								++i;
-								break;
+                                break;
 						}
 						std::vector<std::string> header;
 						boost::split(header, request_lines[i], boost::is_any_of(": "), boost::token_compress_on);
-						headers[header[0]] = header[1];
+						headers.emplace(header[0],header[1]);
 				}
 
 				for(; i < request_lines.size(); ++i) {
