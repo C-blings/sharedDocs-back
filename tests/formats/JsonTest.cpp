dc30e5a53c66e6d167079dc759fd8bea8618bccf
Sliderer <slidedru8gs@mail.ru>
2023-11-30 11:00:09 +0300

    feat tests: add JsonTest

---
+++ tests/formats/JsonTest.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+
+TEST(CheckAllValue, JsonTest){
+    std::string value = "{"
+                        "\"host\": 127.0.0/1,"
+                        "\"post\": 80,"
+                        "}";
+    std::cout << value;
+}
