2780158b49cc778d223b98aeb14071046b5e7607
Pavel Nikulshin <p.nikulshin@ispras.ru>
2023-12-10 19:56:58 +0300

    merge

--- src/database/DatabaseController.hpp
+++ src/database/DatabaseController.hpp
@@ -7,14 +7,6 @@ namespace database{
     template<typename Connection, typename Store>
     class DatabaseController {
     public:
-<<<<<<< HEAD
         virtual std::vector<std::vector<Store>> HandleQuery(Connection& connection,
                                        const std::string& query, const std::vector<Store>& parameters) = 0;
-    };
 }
-=======
-        virtual std::vector<Store> HandleQuery(Connection& connection,
-                                       const std::string& query, const std::vector<Store>& parameters) = 0;
-    };
-}
->>>>>>> 2a1300154042054268781c11227867e0572b8b37
